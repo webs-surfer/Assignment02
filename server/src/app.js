ef8fc45f37ffca8b2f378d8d6ecd31db6104a219
webs-surfer <itsrohanadhikari@gmail.com>
2026-06-28 17:02:14 +0530

    minor changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,30 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import taskRoutes from "./routes/task.routes.js";
 import errorHandler from "./middleware/error.middleware.js";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 
+// API Routes
 app.use("/api/task", taskRoutes);
+
+// Serve React build
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+});
+
+// Error Middleware
 app.use(errorHandler);
 
 export default app;
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import app from './app.js';
 import connectDB from './config/db.js';
 
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
